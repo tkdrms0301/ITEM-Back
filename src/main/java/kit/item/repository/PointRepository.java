7439b6dbacf7596ec31580d07571099e29b4046f
tprud9412 <37810297+tprud9412@users.noreply.github.com>
2023-05-17 00:34:55 +0900

    5/17 회의 - zure 파일 서버 및 포인트 검증로직 추가
    포인트히스토리 삭제 검증로직 추가

--- src/main/java/kit/item/repository/PointRepository.java
+++ src/main/java/kit/item/repository/PointRepository.java
@@ -17,6 +17,9 @@ public interface PointRepository extends JpaRepository<PointHistory, Long> {
     @Query("select new kit.item.dto.entity.point.PointHistoryDto(p.id, p.date, p.point, p.serviceName, p.serviceType) from POINT_HISTORY p where p.member.id=:id")
     List<PointHistoryDto> findPointHistoryByMemberId(@Param("id") Long id);
 
+    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM POINT_HISTORY p WHERE p.member.id = :memberId AND p.id = :historyId")
+    boolean existsByMemberIdAndHistoryId(@Param("memberId") Long memberId, @Param("historyId") Long historyId);
+
     @Query("select new kit.item.dto.entity.point.PointHistoryDto(p.id, p.date, p.point, p.serviceName, p.serviceType) " +
             "from POINT_HISTORY p " +
             "where p.member.id = :memberId " +
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,10 +8,6 @@ VALUES ('경북 구미시 옥계2공단로 310-1', 'test3', '정비사_ID', '정
 INSERT INTO seller(company_address, company_name, company_number, company_phone_number, description, member_id)
 VALUES ('company_address', 'company_name', 'company_number', 'company_phone_number', 'seller description', 2);
 INSERT INTO repair_shop(description, shop_name, shop_phone_number, member_id)
-VALUES ('mechanic description', 'shop_name', 'shop_phone_number', 3);
-
-
-INSERT INTO repair_shop(description, shop_name, shop_phone_number, member_id)
 VALUES ('test description1', 'test shop1', '010-1234-5678', 3);
 INSERT INTO repair_service(name, repair_shop_id)
 VALUES ('test service1', 3);

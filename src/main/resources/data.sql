38df87d0cce4cd62760e8319a75daff228027130
cjfals9626 <cjfals9626@naver.com>
2023-05-17 14:25:43 +0900

    a

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,6 +7,7 @@ VALUES ('경북 구미시 옥계2공단로 310-1', 'test3', '정비사_ID', '정
 
 INSERT INTO seller(company_address, company_name, company_number, company_phone_number, description, member_id)
 VALUES ('company_address', 'company_name', 'company_number', 'company_phone_number', 'seller description', 2);
+
 INSERT INTO repair_shop(description, shop_name, shop_phone_number, member_id)
 VALUES ('test description1', 'test shop1', '010-1234-5678', 3);
 

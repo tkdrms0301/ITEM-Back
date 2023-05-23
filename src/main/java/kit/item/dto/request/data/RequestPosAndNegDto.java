29eb71df86898d0f7e6bb1de3fc871ba7d896aba
tkdrms0301 <tkdrms0301@naver.com>
2023-05-23 17:09:16 +0900

    23/05/23 키워드 빈도수, 제품의 긍/부정 가져오기

---
+++ src/main/java/kit/item/dto/request/data/RequestPosAndNegDto.java
@@ -0,0 +1,12 @@
+package kit.item.dto.request.data;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestPosAndNegDto {
+    private Long productId;
+}

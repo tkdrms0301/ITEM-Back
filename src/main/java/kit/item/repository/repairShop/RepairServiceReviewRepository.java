e68c9571428bc8cd320f561072f7a65419bf8dd5
tkdrms0301 <tkdrms0301@naver.com>
2023-06-01 03:03:15 +0900

    23/06/01

--- src/main/java/kit/item/repository/repairShop/RepairServiceReviewRepository.java
+++ src/main/java/kit/item/repository/repairShop/RepairServiceReviewRepository.java
@@ -14,14 +14,6 @@ import java.util.Optional;
 
 @Repository
 public interface RepairServiceReviewRepository extends JpaRepository<RepairServiceReview, Long> {
-    @Query(value = "select rw.review_id, rw.content, rw.rating, rr.reply_id, rr.content, m.nickname, m2.nickname " +
-            "from repair_service_review rw " +
-            "left join repair_service_reply rr on rw.review_id = rr.review_id " +
-            "left join member m on rw.member_id = m.member_id " +
-            "left join repair_shop rs on rs.member_id = rr.repair_shop_id " +
-            "left join member m2 on rs.member_id = m2.member_id ", nativeQuery = true)
-    Page<Object> findAllByRepairShopIdV2(@Param(value = "shopId") Long shopId, Pageable page);
-
     @Query("SELECT new kit.item.dto.entity.repairShop.RepairServiceReviewDto(rw.id, rw.content, rw.rating, rr.id, rr.content, m.nickname, rs.nickname) " +
             "FROM REPAIR_SERVICE_REVIEW rw " +
             "LEFT JOIN REPAIR_SERVICE_REPLY rr ON rw.id = rr.repairServiceReview.id " +
